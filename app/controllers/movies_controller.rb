eef8c0de7fb77241b27d6f6268a2dae3e9abf4b2
Sabine <matisuo@gmail.com>
2018-02-15 18:46:03 +0100

    full app push

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,28 @@
+class MoviesController < ApplicationController
+
+  def index
+  end
+
+  def search
+
+  	@search = search_params[:movie]
+
+
+  	@movies = SearchMovie.new(@search).perform
+
+  	@img = "https://image.tmdb.org/t/p/w500/"
+
+  	
+
+
+  end
+
+  private 
+
+  def search_params
+
+  	params.require(:search).permit(:movie)
+
+  end
+
+end
---
+++ app/services/search_movie.rb
@@ -0,0 +1,33 @@
+class SearchMovie
+
+    def initialize(request)
+
+        @search = request
+
+    end
+
+    def perform
+
+        puts "connecting"
+
+        Tmdb::Api.key(ENV['THEMOVIEDB_API_KEY'])
+
+        puts "searching"
+        @result =    Tmdb::Search.movie(@search).results
+
+        @result.each do |movie|
+            directed(movie)
+        end
+
+    end
+
+    def directed(movie)
+
+         Tmdb::Movie.director(movie.id).each do |director|
+             movie[:director] = director.name        
+         end   
+
+    end
+
+
+end
